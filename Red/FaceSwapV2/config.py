9d0ab9ad790fd9e8cab6dbc6a2b9240486269f35
waliddo <79306535+waliddo@users.noreply.github.com>
2022-10-12 14:38:28 +0200

    Improved faceswap version

---
+++ Red/FaceSwapV2/config.py
@@ -0,0 +1,6 @@
+PREDICTOR_PATH = 'models/shape_predictor_68_face_landmarks.dat'
+DATASET_DIR = 'imgs/celeba_hq/train/male'
+FACESWAP_DIR = 'faceswaps'
+#REFERENCE_DIR = 'reference_img'
+IMG_SIZE = 1024
+REFERENCE_IMG = 'imgs/hillary_clinton.jpeg'
